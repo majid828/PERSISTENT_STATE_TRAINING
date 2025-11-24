89ffaa3b17fa9d7f28397f1f11184a7f39b50dfd
majid828 <mhshah1@crimson.ua.edu>
2025-11-24 02:36:47 -0600

    Update scalers.py

--- utils/scalers.py
+++ utils/scalers.py
@@ -1,1 +1,9 @@
+from data.camels_hourly_loader import scaler_out
+
+
+def denorm_from_scaler(x_std: float) -> float:
+    """Denormalize a single standardized discharge value using global scaler_out."""
+    mu = float(scaler_out.mean_[0])
+    sd = float(scaler_out.scale_[0])
+    return max(x_std * sd + mu, 0.0)
 
